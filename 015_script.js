1c6d258b7960a1d8a4659f52033e9f27da32efa4
Born <yura.yushkevich@gmail.com>
2020-11-16 16:44:04 +0300

    Прошел урок 024 по объектам

--- 015_script.js
+++ 015_script.js
@@ -1,96 +1,78 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-let numberOfFilms;
-
-
-
-
-// start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели','');
+
+        while(personalMovieDB.count == '' || isNaN(personalMovieDB.count) || personalMovieDB.count == null){
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели','');
+        } 
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            let lastFilm = prompt('Один из последних просмотренных фильмов?','');
+            let reitFilm = prompt('На сколько оцените его?','');
+            
+            if (lastFilm !='' && lastFilm !=null && reitFilm !='' && reitFilm !=null){
+                if(lastFilm.length < 50 && reitFilm <50){
+                    personalMovieDB.movies[lastFilm] = reitFilm;
+                } else{
+                    lastFilm = prompt('Один из последних просмотренных фильмов?','');
+                    reitFilm = prompt('На сколько оцените его?','');
+                }        
+            } else{
+                i--;
+            }   
+        }  
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+         console.log ('Просмотрено довольно мало фильмов');
+         } else if(personalMovieDB.count >=10 && personalMovieDB.count <30){
+             console.log ('Вы киноман');
+         }else if(personalMovieDB.count >=30){
+             console.log ('Вы классический зритель');    
+         }
+         else{
+             console.log ('Произошла ошибка');
+         } 
+     },
+    showMyDB: function(hidden) {
+        if (!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <=3;i++) {               
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`,'');
+            if (genre !='' && genre !=null){
+                personalMovieDB.genres[i-1] = genre;
+            }else{
+                genre = prompt(`Ваш любимый жанр под номером ${i}`,'');
+                i--; 
+            }            
+        }
+        personalMovieDB.genres.forEach((item, i) =>{
+            console.log(`Любимый жанр № ${i+1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false; 
+        }else{
+            personalMovieDB.privat = true;  
+        }  
+    }
 };
 
-// detectPersonalLevel();
-// rememberMyFilms();
-writeYourGenres();
-showMyDB(personalMovieDB.privat);
-
-
-
-
-
-
-function start () {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
-    
-    while(numberOfFilms == '' || isNaN(numberOfFilms) || numberOfFilms == null){
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
-    }
-    
-}
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let lastFilm = prompt('Один из последних просмотренных фильмов?','');
-        let reitFilm = prompt('На сколько оцените его?','');
-        
-        if (lastFilm !='' && lastFilm !=null && reitFilm !='' && reitFilm !=null){
-            if(lastFilm.length < 50 && reitFilm <50){
-                personalMovieDB.movies[lastFilm] = reitFilm;
-            } else{
-                lastFilm = prompt('Один из последних просмотренных фильмов?','');
-                reitFilm = prompt('На сколько оцените его?','');
-            }        
-        } else{
-            i--;
-        }   
-    }  
-}
-
-function detectPersonalLevel() {
-   if (personalMovieDB.count < 10) {
-    console.log ('Просмотрено довольно мало фильмов');
-    } else if(personalMovieDB.count >=10 && personalMovieDB.count <30){
-        console.log ('Вы киноман');
-    }else if(personalMovieDB.count >=30){
-        console.log ('Вы классический зритель');    
-    }
-    else{
-        console.log ('Произошла ошибка');
-    } 
-}
-
-
-function showMyDB(hidden) {
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 1; i <=3;i++) {   
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`,'');
-    }
-}
-
-
+// personalMovieDB.start();
+// personalMovieDB.detectPersonalLevel();
+// personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+// personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.showMyDB(personalMovieDB.privat);
--- script.js
+++ script.js
@@ -1,49 +1,26 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const solder = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+      console.log('Hello');  
+    },
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?','');
-let reitFilm = prompt('На сколько оцените его?','');
-
-let lastFilm2 = prompt('Один из последних просмотренных фильмов?','');
-let reitFilm2 = prompt('На сколько оцените его?','');
-
-personalMovieDB.movies[lastFilm] = reitFilm;
-personalMovieDB.movies[lastFilm2] = reitFilm2;
-
-console.log(personalMovieDB);
+const jonh = Object.create(solder);
 
 
 
+// const john = {
+//     health: 100,
+// };
+
+
+// john.__proto__ = solder;
+
+// Object.setPrototypeOf(john,solder);
+
+jonh.sayHello();
 
